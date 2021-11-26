0ad0a4f8f224fedaf1164d689a950a49d5c94df2
BubnivaKsenia <xeniyabubnova@yandex.ru>
2021-11-26 13:52:24 +0300

    Initial Commit

---
+++ kva/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kva
+//
+//  Created by 35кабинет9 on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
